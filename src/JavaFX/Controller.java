6412473ba885086e0639e2b319b93ed9de1fa58b
kim-ly <SoSe2018>
2019-08-06 18:20:27 +0200

    Initial commit

---
+++ src/JavaFX/Controller.java
@@ -0,0 +1,14 @@
+package JavaFX;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+
+    public void generateMaze(ActionEvent event) {
+        System.out.println("Halloo");
+    }
+
+    public void solveMaze(ActionEvent event) {
+        System.out.println("Halloo");
+    }
+}
